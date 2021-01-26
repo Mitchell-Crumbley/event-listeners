d25d7fe8d42311fd96beb8caa7fb045603e0c609
Mitchell Crumbley <mitchellcrumbley@gmail.com>
2021-01-26 17:35:42 -0600

    pie builder completed

--- main.js
+++ main.js
@@ -70,49 +70,57 @@ const printToDom = (divId, textToPrint) => {
 
 const pieBuilder = (taco) => {
   let domString = '';
-
   for (let i = 0; i < taco.length; i++) {
-    domString += `<div class="card my-2" style = "width: 18rem;" id = ${i}>
-    <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
-    <div class="card-body">
-      <p class="card-text">${taco[i].name}</p>
-      <p class="card-text">${taco[i].ingredients}</p>
-      <p class="card-text">${taco[i].bakeTemp}</p>
-      <p class="card-text">${taco[i].drinkPairing}</p>
-      <p class="card-text">${taco[i].iceCream}</p>
-      <button type="button" class="btn btn-danger" id="${i}">Delete</button>
-    </div>
-  </div>`;
+    domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+                    <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+                    <div class="card-body">
+                      <p class="card-text">${taco[i].name}</p>
+                      <p class="card-text">${taco[i].ingredients}</p>
+                      <p class="card-text">${taco[i].bakeTemp}</p>
+                      <p class="card-text">${taco[i].drinkPairing}</p>
+                      <p class="card-text">${taco[i].iceCream}</p>
+                      <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+                    </div>
+                  </div>`;
   }
 
   printToDom('#pies', domString);
-
 }
 
 const handleButtonClick = (e) => {
   const buttonId = e.target.id;
 
+  // CHANGING BG COLOR BASED ON BUTTON CLICK
   if (buttonId === 'Trinity') {
-    //DARK MODE
+    // DARK MODE
     document.querySelector('body').style.backgroundColor = '#000';
   } else if (buttonId === 'Doc') {
-    //LIGHT MODE
+    // LIGHT MODE
     document.querySelector('body').style.backgroundColor = '#FFF';
   } else if (buttonId === 'Aja') {
-    //MEDIUM MODE
+    // MEDIUM MODE
     document.querySelector('body').style.backgroundColor = '#808080';
   } else if (buttonId === 'All') {
-    //DEFAULT MODE
+    // DEFAULT
     document.querySelector('body').style.backgroundColor = 'rgb(175, 196, 175)';
   }
 
-  const selectedPies = ();
+  // UPDATE THE PIES BASED ON BUTTON CLICKED
+  const selectedPies = [];
+  // pies[0].instructor // 'Doc'
   for (let i = 0; i < pies.length; i++) {
     if (pies[i].instructor === buttonId) {
       selectedPies.push(pies[i]);
     }
   }
-  console.log(selectedPies);
+
+  if (buttonId === 'All') {
+    // PRINT ALL THE PIES
+    pieBuilder(pies);
+  } else {
+    pieBuilder(selectedPies);
+  }
+
 }
 
 const buttonEvents = () => {
@@ -120,8 +128,6 @@ const buttonEvents = () => {
   document.querySelector('#Doc').addEventListener('click', handleButtonClick);
   document.querySelector('#Aja').addEventListener('click', handleButtonClick);
   document.querySelector('#Trinity').addEventListener('click', handleButtonClick);
-
-  allBtn.addEventListener('click', handleButtonClick);
 }
 
 const init = () => {
